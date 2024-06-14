31644a07c0975a8d008026ae230839a8e8a1c79e
Harshita 068 <harshita9654755205@gmail.com>
2024-06-14 11:52:58 +0530

    first commit

--- ques16.py
+++ ques16.py
@@ -1,14 +1,4 @@
 str1=str(input("enter any string : "))
-
-
-'''freq={}
-for i in str1:
-    if i in freq:
-        freq[i]+=1
-    else:
-        freq[i]=1
-print(freq)'''
-
 char=input("enter the character : ")
 freq=str1.count(char)
 if char in str1:
